fe903c83756c80cc61f43d5037fa5821cf770f46
cherlyx <121024972+cherlyx@users.noreply.github.com>
2022-12-25 19:20:09 +0300

    Add files via upload

---
+++ Использование контейнеров вар 33.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+	cout << "Введите последовательность целых чисел, которая кончится отрицательным числом ";
+
+	vector<int> numbers;
+
+	int n;
+
+	while (cin >> n && n >= 0) {
+
+		numbers.push_back(n);
+
+	}
+
+	cout << "Введите k-ый элемент перед которым будет стоять 0: ";
+
+	int k;
+
+	cin >> k;
+
+	for (int i = k - 1; i < numbers.size(); i += k) {
+
+		numbers.insert(numbers.begin() + i, 0);
+
+	}
+
+	cout << "Последовательность с 0 после K-ого элемента: ";
+
+	for (int number : numbers) {
+
+		cout << number << " ";
+
+	}
+
+	return 0;
+
+}
