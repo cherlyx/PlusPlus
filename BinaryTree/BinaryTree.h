f2d30b4a0f4b40c1d3c809008c5d85792ee89f33
cherlyx <121024972+cherlyx@users.noreply.github.com>
2022-12-29 22:21:43 +0300

    Update BinaryTree.h

--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -3,6 +3,10 @@
 
 class BinaryTree
 {
+	vector<int> getVerticesSmallerThan(int value) {
+        vector<int> vertices;
+        getVerticesSmallerThan(root, value, vertices);
+        return vertices;
 	TreeElement* root;
 	TreeElement* findElement(int n);
 	TreeElement* recursiveFindElement(int n);
@@ -15,7 +19,16 @@ public:
 
 	void add(int current_n,int new_n);
 	void remove(int n);
-	
+private:
+	void getVerticesSmallerThan(Node* node, int value, std::vector<int>& vertices) {
+        if (node == nullptr) {
+            return;
+        }
 
-
+        getVerticesSmallerThan(node->left, value, vertices);
+        if (node->value < value) {
+            vertices.push_back(node->value);
+        }
+        getVerticesSmallerThan(node->right, value, vertices);
+    }	
 };
